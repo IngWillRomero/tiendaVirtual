4f26cfd9d0d7081cdd1d0493848dd5b66a2e315c
CASCADA <villiam1a10@hotmail.com>
2015-11-28 17:16:04 -0500

    relaciones y campos en los modelos User, Article, Category

--- app/Article.php
+++ app/Article.php
@@ -7,4 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     //
+	protected $table = "articles" ;
+
+	protected $fillable = ['title','content','user_id','category_id','path'] ;
+
+
+	public function user()
+	{
+		return $this->belongsTo('App\User') ;
+	}
+
+	public function category()
+	{
+		return $this->belongsTo('App\Category') ;
+	}
+
+
+
 }
--- app/Category.php
+++ app/Category.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     //
+
+	protected $table = "categories" ;
+
+	protected $fillable = ['name'] ;
+
+
+
+    public function articles() 
+    {
+    	return $this->hasMany('App\Article') ;
+    }
+    
 }
